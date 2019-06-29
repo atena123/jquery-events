d0fb9093a32c12e34d4cbbc976ec21ac157efead
Aneta Kobylarz <atena771@wp.pl>
2019-06-29 22:26:07 +0000

    jquery-events

---
+++ script.js
@@ -0,0 +1,109 @@
+$(document).ready(function() {
+    
+   
+    $("#stream1_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream1").addClass('highlight_stream');
+   });
+   $("#stream2_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream2").addClass('highlight_stream');
+   });
+   $("#stream3_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream3").addClass('highlight_stream');
+   });
+   
+    $(".bottom_button").mouseenter(function(){
+        $("body").css( "background-color", "black"); 
+    });
+
+    $(".bottom_button").mouseleave(function(){
+        $("body").css( "background-color", "#eee"); 
+    });
+    
+     $("h2").hover(function(){
+        $("h2").addClass( "h2_color");   
+    });
+    
+     $("p").click(function(){
+        $("p").addClass("highlight_text");
+    });
+    
+     $("#hr_html").hover(function(){
+        $("#hr_mysql").removeClass("h2_font_size");
+        $("#hr_python").removeClass("h2_font_size");
+        $("#hr_jquery").removeClass("h2_font_size");
+        $("#hr_django").removeClass("h2_font_size");
+        $("#hr_css").removeClass("h2_font_size");
+        $("#hr_mysql").removeClass("h2_font_size");
+        $("#hr_html").addClass("h2_font_size");
+    });
+
+    $("#hr_mysql").hover(function(){
+        $("#hr_python").removeClass("h2_font_size");
+        $("#hr_jquery").removeClass("h2_font_size");
+        $("#hr_django").removeClass("h2_font_size");
+        $("#hr_css").removeClass("h2_font_size");
+        $("#hr_html").removeClass("h2_font_size");
+        $("#hr_mysql").addClass("h2_font_size");
+    });
+
+    $("#hr_python").hover(function(){
+        $("#hr_mysql").removeClass("h2_font_size");
+        $("#hr_jquery").removeClass("h2_font_size");
+        $("#hr_django").removeClass("h2_font_size");
+        $("#hr_css").removeClass("h2_font_size");
+        $("#hr_html").removeClass("h2_font_size");
+        $("#hr_python").addClass("h2_font_size");
+    });
+
+    $("#hr_jquery").hover(function(){
+        $("#hr_mysql").removeClass("h2_font_size");
+        $("#hr_python").removeClass("h2_font_size");
+        $("#hr_django").removeClass("h2_font_size");
+        $("#hr_css").removeClass("h2_font_size");
+        $("#hr_html").removeClass("h2_font_size");
+        $("#hr_jquery").addClass("h2_font_size");
+    });
+
+    $("#hr_django").hover(function(){
+        $("#hr_mysql").removeClass("h2_font_size");
+        $("#hr_python").removeClass("h2_font_size");
+        $("#hr_jquery").removeClass("h2_font_size");
+        $("#hr_css").removeClass("h2_font_size");
+        $("#hr_html").removeClass("h2_font_size");
+        $("#hr_django").addClass("h2_font_size");
+    });
+
+    $("#hr_css").hover(function(){
+        $("#hr_mysql").removeClass("h2_font_size");
+        $("#hr_python").removeClass("h2_font_size");
+        $("#hr_jquery").removeClass("h2_font_size");
+        $("#hr_django").removeClass("h2_font_size");
+        $("#hr_html").removeClass("h2_font_size");
+        $("#hr_css").addClass("h2_font_size");
+    });
+    
+    $("#bottom_button_1").click(function(){
+        $("#card_para_1").toggle(1000);
+    });
+    
+    $("#bottom_button_2").click(function(){
+        $("#card_para_2").slideToggle(1000);
+    });
+    
+    $("#select_btn").click(function() {
+		$(".card:not(.highlight)").hide();	 
+	});
+
+    
+     
+});
+
